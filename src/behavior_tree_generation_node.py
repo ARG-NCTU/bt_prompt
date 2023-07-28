e95fbdf0bd64e39878182d125e00793534d28d53
bawkbak <hsiehbawkbak@gmail.com>
2023-07-28 17:49:38 +0800

    Feat: Add pop up window to visulaize prompt

--- src/behavior_tree_generation_node.py
+++ src/behavior_tree_generation_node.py
@@ -67,7 +67,7 @@ class Pipeline:
         self.rs_selector = tk.OptionMenu(self.middle_frame, self.rs_variable, *self.options)
         self.rs_selector.config(height=2, width=15, font=('Helvatical bold',20)) 
 
-        self.sb_label = tk.Label(self.middle_frame, text="Prompts about source behavior tree: ")
+        self.sb_label = tk.Label(self.middle_frame, text="Prompts about source behavior trees: ")
         self.sb_label.config(font=('Helvatical bold',20))
 
         self.sb_variable = tk.StringVar(self.middle_frame)
@@ -81,25 +81,34 @@ class Pipeline:
         self.es_variable.set(self.options[1])
         self.es_selector = tk.OptionMenu(self.middle_frame, self.es_variable, *self.options)
         self.es_selector.config(height=2, width=15, font=('Helvatical bold',20)) 
-        self.ed_label_2 = tk.Label(self.middle_frame, text="Current prompt:")
-        self.ed_label_2.config(font=('Helvatical bold',20))
-        self.ed_text_view = tk.Text(self.middle_frame, height=4, width=40, font=("Arial", 20))
-        
-        self.rs_label_2 = tk.Label(self.middle_frame, text="Current prompt:")
-        self.rs_label_2.config(font=('Helvatical bold',20))
-        self.rs_text_view = tk.Text(self.middle_frame, height=4, width=40, font=("Arial", 20))
-        
-        self.sb_label_2 = tk.Label(self.middle_frame, text="Current prompt:")
-        self.sb_label_2.config(font=('Helvatical bold',20))
-        self.sb_text_view = tk.Text(self.middle_frame, height=4, width=40, font=("Arial", 20))
-        
-        self.es_label_2 = tk.Label(self.middle_frame, text="Current prompt:")
-        self.es_label_2.config(font=('Helvatical bold',20))
-        self.es_text_view = tk.Text(self.middle_frame, height=4, width=40, font=("Arial", 20))
-        
 
+        self.ed_label_2 = tk.Label(self.middle_frame, text="Current prompt about environment descriptions:")
+        self.ed_label_2.config(font=('Helvatical bold',20))
+        
+        
+        self.rs_label_2 = tk.Label(self.middle_frame, text="Current prompt about robot sensing abilities:")
+        self.rs_label_2.config(font=('Helvatical bold',20))
+
+        self.sb_label_2 = tk.Label(self.middle_frame, text="Current prompt about source behavior trees:")
+        self.sb_label_2.config(font=('Helvatical bold',20))
+
+        
+        self.es_label_2 = tk.Label(self.middle_frame, text="Current prompt about existing sub trees:")
+        self.es_label_2.config(font=('Helvatical bold',20))
+
+        
         self.task_button = tk.Button(self.lower_frame, text="Submit", command=self.task_submit)
         self.task_button.config(height=2, width=15, font=('Helvatical bold',20)) 
+
+
+        self.view_prompt_ed_button = tk.Button(self.middle_frame, text="View detail and edit", command=self.view_prompt_ed)
+        self.view_prompt_ed_button.config(height=2, width=15, font=('Helvatical bold',20)) 
+        self.view_prompt_rs_button = tk.Button(self.middle_frame, text="View detail and edit", command=self.view_prompt_rs)
+        self.view_prompt_rs_button.config(height=2, width=15, font=('Helvatical bold',20)) 
+        self.view_prompt_sb_button = tk.Button(self.middle_frame, text="View detail and edit", command=self.view_prompt_sb)
+        self.view_prompt_sb_button.config(height=2, width=15, font=('Helvatical bold',20)) 
+        self.view_prompt_es_button = tk.Button(self.middle_frame, text="View detail and edit", command=self.view_prompt_es)
+        self.view_prompt_es_button.config(height=2, width=15, font=('Helvatical bold',20)) 
 
     def ui_pack(self):
         self.upper_frame.pack()
@@ -116,44 +125,24 @@ class Pipeline:
         self.es_label.grid(row=6, column=0, padx=5, pady=5)
         self.es_selector.grid(row=7, column=0, padx=5, pady=5)
         self.ed_label_2.grid(row=0, column=1, padx=5, pady=5)
-        self.ed_text_view.grid(row=1, column=1, padx=5, pady=5)
+        self.view_prompt_ed_button.grid(row=1, column=1, padx=5, pady=5)
         self.rs_label_2.grid(row=2, column=1, padx=5, pady=5)
-        self.rs_text_view.grid(row=3, column=1, padx=5, pady=5)
+        self.view_prompt_rs_button.grid(row=3, column=1, padx=5, pady=5)
         self.sb_label_2.grid(row=4, column=1, padx=5, pady=5)
-        self.sb_text_view.grid(row=5, column=1, padx=5, pady=5)
+        self.view_prompt_sb_button.grid(row=5, column=1, padx=5, pady=5)
         self.es_label_2.grid(row=6, column=1, padx=5, pady=5)
-        self.es_text_view.grid(row=7, column=1, padx=5, pady=5)
+        self.view_prompt_es_button.grid(row=7, column=1, padx=5, pady=5)
 
         self.lower_frame.pack()
         self.task_button.pack()
 
-        # self.tab = tk.Label(self.lower_frame, text="\n")
-        # self.tab.pack()
-        # # self.tab.grid(row=9, column=0, padx=5, pady=5)
-
-        # self.text_result = tk.Label(self.lower_frame, text="Submit prompts to start generate.")
-        # self.text_result.config(font=('Helvatical bold',20))
-        # self.text_result.pack()
-        # self.text_result.grid(row=10, column=0, padx=5, pady=5)
-
     def get_single_prompt(self, root_dir, file_name):
         file_path = root_dir + file_name
-        print(file_path)
+        # print(file_path)
         prompt = ""
         with open(file_path, 'r') as file:
             prompt = file.read().rstrip()
         return prompt
-
-    def get_prompts(self):
-        self.text_environment_descriptions = self.get_single_prompt(self.prompt_dir, "environment_description.txt")
-        self.text_robot_sensing = self.get_single_prompt(self.prompt_dir, "robot_sensing_abilities.txt")
-        self.text_source_bt = self.get_single_prompt(self.prompt_dir, "source_behavior_tree.txt")
-        self.text_existing_subtree = self.get_single_prompt(self.prompt_dir, "existing_sub_trees.txt")
-
-        self.ed_text_view.insert(tk.END, self.text_environment_descriptions)
-        self.rs_text_view.insert(tk.END, self.text_robot_sensing)
-        self.sb_text_view.insert(tk.END, self.text_source_bt)
-        self.es_text_view.insert(tk.END, self.text_existing_subtree)
 
     def input_process(self, input):
         if input == "Enable":
@@ -166,7 +155,7 @@ class Pipeline:
     def LLM_generation(self):
         openai.api_key = self.api_key
         response = openai.Completion.create(
-            model=self.model,
+            model=self.model_name,
             prompt=self.final_prompt,
             temperature=0,
             max_tokens=500,
@@ -176,13 +165,90 @@ class Pipeline:
         )
         raw_string = remove_front_until(str(response['choices'][0]['text']), "->")
 
-        raw_text_file = open("result/output.tree", "wt")
+        raw_path = self.generate_file + "raw.txt"
+        full_path = self.generate_file + "test.tree"
+
+        raw_text_file = open(raw_path, "wt")
         n = raw_text_file.write(raw_string)
         raw_text_file.close()
 
-        text_file = open(self.generate_file, "wt")
+        text_file = open(full_path, "wt")
         m = text_file.write(subtree_assembly(raw_string))
         text_file.close()
+
+
+    def view_prompt_ed(self):
+        def on_popup_window_close():
+            self.input_environment_descriptions = text_label.get("1.0", tk.END).strip() + "\n"
+            popup_window.destroy()
+        
+        popup_window = tk.Toplevel(self.window)
+        popup_window.title("Prompt about environment descriptions")
+
+        text_label = tk.Text(popup_window, height=30, width=120, font=("Arial", 20))
+        text_label.pack()
+
+        save_button = tk.Button(popup_window, text="Save and Close", command=on_popup_window_close)
+        save_button.config(height=2, width=15, font=('Helvatical bold',20)) 
+        save_button.pack()
+        
+        text_label.insert(tk.END, self.get_single_prompt(self.prompt_dir, "environment_description.txt"))
+
+        
+
+    def view_prompt_es(self):
+        def on_popup_window_close():
+            self.input_existing_subtree = text_label.get("1.0", tk.END).strip() + "\n"
+            popup_window.destroy()
+
+        popup_window = tk.Toplevel(self.window)
+        popup_window.title("Prompt about existing sub trees")
+
+        text_label = tk.Text(popup_window, height=30, width=120, font=("Arial", 20))
+        text_label.pack()
+
+        save_button = tk.Button(popup_window, text="Save and Close", command=on_popup_window_close)
+        save_button.config(height=2, width=15, font=('Helvatical bold',20)) 
+        save_button.pack()
+
+        text_label.insert(tk.END, self.get_single_prompt(self.prompt_dir, "existing_sub_trees.txt"))
+
+
+    def view_prompt_rs(self):
+        def on_popup_window_close():
+            self.input_robot_sensing = text_label.get("1.0", tk.END).strip() + "\n"
+            popup_window.destroy()
+
+        popup_window = tk.Toplevel(self.window)
+        popup_window.title("Prompt about robot sensing abilities")
+
+        text_label = tk.Text(popup_window, height=30, width=120, font=("Arial", 20))
+        text_label.pack()
+
+        save_button = tk.Button(popup_window, text="Save and Close", command=on_popup_window_close)
+        save_button.config(height=2, width=15, font=('Helvatical bold',20)) 
+        save_button.pack()
+
+        text_label.insert(tk.END, self.get_single_prompt(self.prompt_dir, "robot_sensing_abilities.txt"))
+
+        
+
+    def view_prompt_sb(self):
+        def on_popup_window_close():
+            self.input_source_bt = text_label.get("1.0", tk.END).strip() + "\n"
+            popup_window.destroy()
+
+        popup_window = tk.Toplevel(self.window)
+        popup_window.title("Prompt about existing sub trees")
+
+        text_label = tk.Text(popup_window, height=30, width=120, font=("Arial", 20))
+        text_label.pack()
+
+        save_button = tk.Button(popup_window, text="Save and Close", command=on_popup_window_close)
+        save_button.config(height=2, width=15, font=('Helvatical bold',20)) 
+        save_button.pack()
+
+        text_label.insert(tk.END, self.get_single_prompt(self.prompt_dir, "source_behavior_tree.txt"))
 
 
     def task_submit(self):
@@ -194,36 +260,26 @@ class Pipeline:
 
         self.input_task = self.text.get("1.0", tk.END).strip()
 
-        if self.status_environment_descriptions == True:
-            self.input_environment_descriptions = self.ed_text_view.get("1.0", tk.END).strip() + "\n"
+        if self.status_environment_descriptions == False:
+            self.input_environment_descriptions = ""
 
-        if self.status_robot_sensing == True:  
-            self.input_robot_sensing = self.rs_text_view.get("1.0", tk.END).strip() + "\n"
+        if self.status_robot_sensing == False:  
+            self.input_robot_sensing = ""
 
-        if self.status_source_bt == True:
-            self.input_source_bt = self.sb_text_view.get("1.0", tk.END).strip() + "\n"
+        if self.status_source_bt == False:
+            self.input_source_bt = ""
 
-        if self.status_existing_subtree == True:
-            self.input_existing_subtree = self.es_text_view.get("1.0", tk.END).strip() + "\n"
+        if self.status_existing_subtree == False:
+            self.input_existing_subtree = ""
         
-        # print("Input:", str(self.input_task))
-        # print("ED: ", self.status_environment_descriptions)
-        # print("RS: ", self.status_robot_sensing)
-        # print("SB: ", self.status_source_bt)
-        # print("ES: ", self.status_existing_subtree)
-
-        # print("ED_T: ", self.input_environment_descriptions)
-        # print("RS_T: ", self.input_robot_sensing)
-        # print("SB_T: ", self.input_source_bt)
-        # print("ES_T: ", self.input_existing_subtree)
 
         self.final_prompt = self.input_environment_descriptions + \
                             self.input_robot_sensing + \
-                            self.input_source_bt + \
                             self.input_existing_subtree + \
+                            self.input_source_bt + \
                             self.input_task 
 
-        # print(self.final_prompt)
+        print(self.final_prompt)
         # call api
         self.LLM_generation()
 
@@ -238,31 +294,29 @@ class Pipeline:
         self.window.mainloop()
 
 
-
-
 if __name__ == '__main__':
-
     rospy.init_node('behavior_tree_generation_node')
     rospack = rospkg.RosPack()
 
-    generation_file = rospack.get_path('behavior_tree') + "/config/drone_latching/test.tree"
+    generate_file = rospack.get_path('behavior_tree') + "/config/drone_latching/"
     prompt_dir = rospack.get_path('behavior_tree_generation') + "/config/prompt/"
     sub_tree_dir = rospack.get_path('behavior_tree_generation') + "/config/subtree/"
     api_key = rospy.get_param('~api_key', '')
     trigger_rosparam = rospy.get_param('~trigger_rosparam', '')
     model_name = rospy.get_param('~model_name', '')
+
+    print(api_key)
    
     rospy.set_param(trigger_rosparam, False)
 
     bt_generation = Pipeline(
-        generation_file, 
+        generate_file, 
         prompt_dir, 
         sub_tree_dir, 
         api_key, 
         model_name
     )
     bt_generation.ui_pack()
-    bt_generation.get_prompts()
     bt_generation.mainloop()
 
     
